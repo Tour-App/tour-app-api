0df662198d5cb7c09d03864b4c21971a84ca628a
Misael Calvillo Mancilla <misa@homely.mx>
2022-05-04 19:14:14 -0500

    Add main scafolding

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('App iniciada!');
+const express = require('express');
+const app = express();
+const PORT = 5001;
+
+app.get('/', (req, res) => {
+  res.send('Server activo ✅');
+})
+
+app.listen(PORT, () => console.log(`Èl servidor inicio en el puerto ${PORT}`))
