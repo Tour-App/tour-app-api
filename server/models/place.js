8231b9982c2d5d6a0dd62debab4a89f92a7fa772
Brenda Nahomi Quispe Gonzales <adnerbi.1107@gmail.com>
2022-05-10 21:32:45 -0500

    Delete unused files

--- server/models/place.js
+++ server/models/place.js
@@ -1,5 +1,5 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class place extends Model {
@@ -12,69 +12,67 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  place.init({
-    id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
+  place.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      admin_name: {
+        type: DataTypes.STRING,
+      },
+      admin_email: {
+        type: DataTypes.STRING,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      address_id: {
+        type: DataTypes.STRING,
+      },
+      type: {
+        type: DataTypes.ENUM,
+        values: ["restaurant", "museum", "park", "historic"],
+        default: null,
+        allowNull: true,
+      },
+      logo: {
+        type: DataTypes.STRING,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      phone_number: {
+        type: DataTypes.STRING,
+      },
+      facebook_url: {
+        type: DataTypes.STRING,
+      },
+      instagram_url: {
+        type: DataTypes.STRING,
+      },
+      web_page_url: {
+        type: DataTypes.STRING,
+      },
+      twitter_url: {
+        type: DataTypes.STRING,
+      },
+      default_photo_id: {
+        type: DataTypes.STRING,
+      },
+      city_id: {
+        type: DataTypes.STRING,
+      },
     },
-    admin_name: {
-      type: Sequelize.STRING
-    },
-    admin_email: {
-      type: Sequelize.STRING
-    },
-    name: {
-      type: Sequelize.STRING
-    },
-    address_id: {
-      type: Sequelize.STRING
-    },
-    type: {
-      type: Sequelize.ENUM,
-      values: [
-        'restaurant',
-        'museum',
-        'park',
-        'historic',
-      ],
-      default: null,
-      allowNull: true
-    },
-    logo: {
-      type: Sequelize.STRING
-    }, 
-    description: {
-      type: Sequelize.STRING,
-    },
-    phone_number: {
-      type: Sequelize.STRING,
-    },
-    facebook_url: {
-      type: Sequelize.STRING,
-    },
-    instagram_url: {
-      type: Sequelize.STRING,
-    },
-    web_page_url: {
-      type: Sequelize.STRING,
-    },
-    twitter_url: {
-      type: Sequelize.STRING,
-    },
-    default_photo_id: {
-      type: Sequelize.STRING,
-    },
-    city_id: {
-      type: Sequelize.STRING,
-    },
-  }, {
-    sequelize,
-    modelName: 'place',
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-  });
+    {
+      sequelize,
+      modelName: "place",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
   return place;
 };
