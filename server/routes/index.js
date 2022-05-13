dc286408a2f55e539a83cac483f35f8b7ec1c080
Misael Calvillo Mancilla <misa@homely.mx>
2022-05-12 19:44:35 -0500

    Add morgan to log routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-
 const router = express.Router();
-
 
 const user = require("../controllers/userController");
 const place = require("../controllers/placesController");
-
 const city = require('../controllers/cityController')
 
+const auth = require('../middlewares/authenticationMiddleware');
 
 router.get('/users', user.getAll);
 router.post('/users', user.create);
@@ -15,12 +13,11 @@ router.post('/users/:id', user.update);
 router.get('/users/:id', user.getOne);
 router.delete('/users/:id', user.delete);
 
+router.post("/places", auth, place.create);
 router.get("/places", place.getAll);
-router.post("/places", place.create);
 router.post("/places/:id", place.update);
 router.get("/places/:id", place.getOne);
 router.delete("/places/:id", place.delete);
-
 
 router.get('/cities', city.getAll);
 router.get('/cities/:id', city.getOne);
